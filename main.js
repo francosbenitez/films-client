a056febdc810d840f0417fa4a6663fe9db32b5f8
francosbenitez <francosbenitez@gmail.com>
2021-09-30 22:58:51 -0300

    added update method

--- main.js
+++ main.js
@@ -24,6 +24,13 @@ class ApiService {
         return response.json()
     }
 
+    async update(pFilmId) {
+        const requestOptions = {
+            method: "PUT"
+        };
+        const response = await fetch(`${this.baseUrl}/${pFilmId}`, requestOptions);
+        return response.json();
+    }
 }
 
 const app = new Vue({
@@ -31,20 +38,15 @@ const app = new Vue({
     data: {
         films: [],
         apiService: null,
-        newFilm: {}
+        newFilm: {},
+        updateFilm: {}
     },
     async created() {
-        //fetch("http://localhost:3000/api/films")
-        //.then(response => response.json())
-        //.then(json => this.films = json)
-        //.catch(err => console.log(err));
         this.apiService = new ApiService();
         this.films = await this.apiService.getAll();
     },
     methods: {
         onClickSendFilm: async function() {
-            //console.log("FUNSIONA");
-            // console.log(this.newFilm)
             await this.apiService.create(this.newFilm);
             this.films = await this.apiService.getAll();
             this.newFilm = {};
@@ -52,6 +54,10 @@ const app = new Vue({
         onClickEliminar: async function(pFilmId) {
             await this.apiService.delete(pFilmId);
             this.films = await this.apiService.getAll();
+        },
+        onClickUpdate: async function(pFilmId) {
+            await this.apiService.update(pFilmId);
+            console.log(pFilmId)
         }
     }
 })
